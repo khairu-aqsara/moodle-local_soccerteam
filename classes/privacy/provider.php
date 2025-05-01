1caa37cdb0c92d6a89649bb51fda56dd60b97380
Khairu Aqsara <khairu@teruselearning.co.uk>
2025-05-01 11:44:16 +0700

    Add initial implementation of the Soccer Team plugin with privacy, access, language, and version files

---
+++ classes/privacy/provider.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_soccerteam\privacy;
+
+use core_privacy\local\metadata\null_provider;
+
+/**
+ * Privacy Subsystem implementation for local_soccerteam.
+ *
+ * The Soccer Team plugin does store personal user data, but it is not exported
+ * as part of the Privacy API as it represents educational user assignments in a course.
+ *
+ * @package    local_soccerteam
+ * @category   privacy
+ * @copyright  2025 Khairu Aqsara <wenkhairu@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return string
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
